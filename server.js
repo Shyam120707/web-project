12e57d823a97cca2c36f5382612e0215346b0cd4
Shyam120707 <tonysamuel1207@gmail.com>
2026-08-14 14:55:00 +0530

    updating

--- server.js
+++ server.js
@@ -9,3 +9,24 @@ app.use(express.static("frontend"))
 app.listen(port,function (){
     console.log("my app is running at http://localhost:"+port)
 })
+ const user=[{
+    "id": 1,
+    "name":"Mr Mathias Møller",
+    "gender" : "male",
+    "image": "https://randomuser.me/api/portraits/men/73.jpg"
+ },
+ {
+    "id":2,
+    "name": "Mr Davut Türkdoğan",
+    "gender":"male",
+    "image":"https://randomuser.me/api/portraits/men/87.jpg",
+ },
+ {
+    "id": 3,
+    "name":"Mr José María Armas",
+    "gender":"male",
+    "image":"https://randomuser.me/api/portraits/men/88.jpg",
+ }
+
+
+]
